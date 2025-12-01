b06b258b7a21d7299c9c82ff9bce1fae77d78a6a
PhilipSBeck <psbeck+1@gmail.com>
2025-12-01 08:12:57 +0000

    2025 day 1

--- Scripts/2025/Day1.js
+++ Scripts/2025/Day1.js
@@ -1,7 +1,56 @@
 function Year2025Day1PartOne(value) {
-    return "undefined";
+    let lines = value.split("\n");
+    let pos = 50;
+    let numZeros = 0;
+    for (let i = 0; i < lines.length; i++) {
+        let num = parseInt(lines[i].substring(1));
+        if (lines[i][0] == "L") {
+            pos -= num;
+            while (pos < 0) {
+                pos += 100;
+            }
+        }
+        else {
+            pos += num;
+            while (pos >= 100) {
+                pos -= 100;
+            }
+        }
+        if (pos == 0){
+            numZeros++;
+        }
+    }
+    return numZeros;
 }
 
 function Year2025Day1PartTwo(value) {
-    return "undefined";
+    let lines = value.split("\n");
+    let pos = 50;
+    let numZeros = 0;
+    for (let i = 0; i < lines.length; i++) {
+        let num = parseInt(lines[i].substring(1));
+        if (lines[i][0] == "L") {
+            for (let i = 0; i < num; i++){
+                pos--;
+                if (pos < 0) {
+                    pos += 100;
+                }
+                if (pos == 0) {
+                    numZeros++;
+                }
+            }
+        }
+        else {
+            for (let i = 0; i < num; i++){
+                pos++;
+                if (pos >= 100) {
+                    pos -= 100;
+                }
+                if (pos == 0) {
+                    numZeros++;
+                }
+            }
+        }
+    }
+    return numZeros;
 }
